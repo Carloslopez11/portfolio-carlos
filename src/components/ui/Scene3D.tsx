1969ce4e408a098f42912624186a29a0918d10ad
Carloslopez 11 <javibillo28@gmail.com>
2026-07-20 16:18:57 -0300

    feat: added meteorites effect

--- src/components/ui/Scene3D.tsx
+++ src/components/ui/Scene3D.tsx
@@ -33,12 +33,50 @@ function ParticleCloud({ ...props }) {
   );
 }
 
+function ShootingStars() {
+  const count = 7;
+  const starsRef = useRef<any[]>([]);
+
+  useFrame((state, delta) => {
+    starsRef.current.forEach((star) => {
+      if (star) {
+        star.position.x -= delta * star.userData.speed;
+        star.position.y -= delta * star.userData.speed;
+        
+        if (star.position.x < -2 || star.position.y < -2) {
+          star.position.x = 2 + Math.random() * 3;
+          star.position.y = 2 + Math.random() * 3;
+          star.userData.speed = 1.5 + Math.random() * 2;
+        }
+      }
+    });
+  });
+
+  return (
+    <group>
+      {Array.from({ length: count }).map((_, i) => (
+        <mesh
+          key={i}
+          ref={(el) => (starsRef.current[i] = el)}
+          position={[2 + Math.random() * 3, 2 + Math.random() * 3, -1 - Math.random() * 2]}
+          userData={{ speed: 1.5 + Math.random() * 2 }}
+          rotation={[0, 0, Math.PI / 4]} 
+        >
+          <capsuleGeometry args={[0.003, 0.3, 4, 8]} />
+          <meshBasicMaterial color="#34d399" transparent opacity={0.6} />
+        </mesh>
+      ))}
+    </group>
+  );
+}
+
 export default function Scene3D() {
   return (
     <div className="fixed inset-0 z-[-1] bg-black">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <ParticleCloud />
+          <ShootingStars />
         </Suspense>
       </Canvas>
     </div>
